90c332a764bdb16c7597e5974f180c0729b913f1
akselis <gustas.akstinas@gmail.com>
2026-09-25 11:58:28 +0300

    updated receiver

--- lab3/processes.py
+++ lab3/processes.py
@@ -1,33 +1,37 @@
-from multiprocessing.connection import PipeConnection, Connection
+from multiprocessing.connection import Connection
 import os
 import time as t
 from datetime import datetime
 from gpiozero import CPUTemperature
 
-def send_temperature(interval: float, send_conn: PipeConnection):
+def send_temperature(interval: float, send_conn: Connection):
     if send_conn.closed:
         print(f"[{os.getpid()}] Error: connection closed. Terminating")
         return
 
+    cpu = CPUTemperature()
     for _ in range(0, 10):
-        temp = CPUTemperature.temperature
+        temp = cpu.temperature
         send_conn.send(temp)
         t.sleep(interval)
 
     send_conn.send(None)
     send_conn.close()
 
-def read_temperature(recv_conn: PipeConnection):
+def read_temperature(recv_conn: Connection):
     if recv_conn.closed:
         print(f"[{os.getpid()}] Error: connection closed. Terminating")
         return
 
-    temp = recv_conn.recv()
-    if temp is None:
-        print(f"[{os.getpid()}] Error: sentinel value received. Terminating")
-        recv_conn.close()
-    if not isinstance(temp, property):
-        print(f"[{os.getpid()}] Error: temperature expected. Terminating")
-        recv_conn.close()
+    while True:
+        temp = recv_conn.recv()
+        if temp is None:
+            print(f"[{os.getpid()}] Error: sentinel value received. Terminating")
+            recv_conn.close()
+            return
+        if not isinstance(temp, float):
+            print(f"[{os.getpid()}] Error: temperature expected. Terminating")
+            recv_conn.close()
+            return
 
-    print(f"[{os.getpid()}] {datetime.now():%I:%M:%S %p} \t\t {temp} *C")
+        print(f"[{os.getpid()}] {datetime.now():%I:%M:%S %p} \t\t {temp} *C")
